e9b004209ff32389580e2ca320dfe27c6bef6231
diogondo <diogo.nunes.oliveira@gmail.com>
2016-03-28 11:48:16 -0300

    teessfe

---
+++ MeuCarroCerto/MeuCarroCerto/Controllers/AdministrarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeuCarroCerto.Controllers
+{
+    public class AdministrarController : Controller
+    {
+        //
+        // GET: /Administrar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
