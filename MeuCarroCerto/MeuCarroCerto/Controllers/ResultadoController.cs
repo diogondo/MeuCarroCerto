225a26eec711b1670ab99215298d0a701af0dcec
diogondo <diogo.nunes.oliveira@gmail.com>
2016-05-11 14:02:38 -0300

    ok

--- MeuCarroCerto/MeuCarroCerto/Controllers/ResultadoController.cs
+++ MeuCarroCerto/MeuCarroCerto/Controllers/ResultadoController.cs
@@ -16,9 +16,12 @@ namespace MeuCarroCerto.Controllers
         public ActionResult Index(Alternativas alternativas)
         {
             
-            
+            MeuCarroCerto.Models.Filtros filtros = new MeuCarroCerto.Models.Filtros();
             List<t_carros> carros = new List<t_carros>();
-            var teste = db.t_carros.SqlQuery("select * from t_carros;");
+            BancoSession bd = BancoSession.CriarConexaoBancoSession();
+            filtros = bd.ListaFiltros[0];
+
+            var teste = db.t_carros.SqlQuery("select * from t_carros where valor between " + filtros.valor1 + " and " + filtros.valor2 + " and ano between " + filtros.ano1 + " and " + filtros.ano2 + " and qtd_ocupantes=" + filtros.qtd_ocupante + " and codigo_tipo_carroceria = " + filtros.tipo_carroceria + ";");
             ViewBag.alternativas = alternativas;
             ViewBag.cont=0;
             foreach (var item in teste)
--- MeuCarroCerto/MeuCarroCerto/Models/Filtros.cs
+++ MeuCarroCerto/MeuCarroCerto/Models/Filtros.cs
@@ -9,7 +9,8 @@ namespace MeuCarroCerto.Models
     {
         public int qtd_ocupante { get; set; }    
         public string tipo_carroceria{ get; set; }
-        public string valor { get; set; }
+        public string valor1 { get; set; }
+        public string valor2 { get; set; }
         public int ano1 { get; set; }
         public int ano2 { get; set; }
         public Filtros()
